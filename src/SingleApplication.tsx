b3d04098e80c88e10dbb3291e7fd2354ee2e76a5
Ilya Katassonov <ilya.katassonov@gmail.com>
2025-10-16 11:09:25 +0100

    Updated styles and UI

--- src/SingleApplication.tsx
+++ src/SingleApplication.tsx
@@ -1,34 +1,38 @@
 import React from "react";
 import styles from "./SingleApplication.module.css";
+import { TApplicationClientDto } from "./api/fetchApplications";
 
-const SingleApplication = ({ application }) => {
+const SingleApplication = ({ application }: { application: TApplicationClientDto }) => {
+  const { id, company, firstName, lastName, email, loanAmount, dateCreated, expiryDate } = application;
+
   return (
     <div className={styles.SingleApplication}>
       <div className={styles.cell}>
-        <sub>Company</sub>
-        {application.company || "-"}
+        <sub className={styles.cellCaption}>Company</sub>
+        <span className={styles.cellValue}>{company || "-"}</span>
       </div>
       <div className={styles.cell}>
-        <sub>Name</sub>
-        {application.first_name} {application.lastName}
+        <sub className={styles.cellCaption}>Name</sub>
+        <span className={styles.cellValue}>{firstName} {lastName}</span>
       </div>
       <div className={styles.cell}>
-        <sub>Email</sub>
-        {application.email || "-"}
+        <sub className={styles.cellCaption}>Email</sub>
+        <span className={styles.cellValue}>
+          {email ? <a className={styles.emailLink} href={`mailto:${email}`}>{email}</a> : "-"}</span>
       </div>
       <div className={styles.cell}>
-        <sub>Loan Amount</sub>
-        {application.loanAmount}
+        <sub className={styles.cellCaption}>Loan Amount</sub>
+        <span className={styles.cellValue}>{loanAmount}</span>
       </div>
       <div className={styles.cell}>
-        <sub>Application Date</sub>
-        {application.dateCreated}
+        <sub className={styles.cellCaption}>Application Date</sub>
+        <span className={styles.cellValue}>{dateCreated}</span>
       </div>
       <div className={styles.cell}>
-        <sub>Expiry date</sub>
-        {application.expiryDate}
+        <sub className={styles.cellCaption}>Expiry Date</sub>
+        <span className={styles.cellValue}>{expiryDate}</span>
       </div>
-    </div>
+    </div >
   );
 };
 
